92dec4f6fe118bf0ffb6c2adade51eb455ca1d1b
lunaroyster <oneinaquadrillion@live.com>
2019-09-13 12:38:29 -0400

    UI fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import MathJax from 'react-mathjax2';
 import './App.css';
 
@@ -14,6 +14,7 @@ function App() {
   const latexSearch = new Fuse(Commands, searchConfig);
   const [searchTerm, setSearchTerm] = useState('');
   let [searchResult, setSearchResult] = useState([]);
+  let [selectedResult, selectResult] = useState(0);
   let copyToClipboard = async text => {
     try {
       let clipboardPerms = await navigator.permissions.query({name: "clipboard-write"});
@@ -24,10 +25,13 @@ function App() {
       console.log(e);
     }
   }
+  const searchInput = useRef(null);
+  useEffect(()=>searchInput.current.focus(), []);
   let updateSearch = e => {
     let term = e.target.value;
     setSearchTerm(term);
     setSearchResult(latexSearch.search(term));
+    selectResult(0);
   }
   let pressKey = e => {
     if (e.key === 'Enter') {
@@ -38,10 +42,10 @@ function App() {
   }
   return (
     <div className="App">
-      <input onChange={e => updateSearch(e)} value={searchTerm} onKeyPress={pressKey} id="searchBox" placeholder="Describe your math symbol..." />
+      <input onChange={e => updateSearch(e)} value={searchTerm} ref={searchInput} onKeyPress={pressKey} id="searchBox" placeholder="Describe your math symbol..." tabIndex={1} />
       <MathJax.Context input='tex'>
         <div>
-          {searchResult.map(r => (
+          {searchResult.map((r,i) => (
             <div key={r.command} className="result">
               {r.descriptions[0]}
               <code>{r.command}</code>
