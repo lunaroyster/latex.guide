6ea02a47df273197edca0dc255ff71e46ab998a5
lunaroyster <oneinaquadrillion@live.com>
2019-09-14 16:56:37 -0400

    node-sass

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import { Table, TableRow, TableCell, TableBody, Container, TableHead, Snackbar, IconButton } from '@material-ui/core';
 
-import './App.css';
+import './App.scss';
 
 import Commands from './Commands';
 
