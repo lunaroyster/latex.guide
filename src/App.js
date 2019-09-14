d5c8ce1b6dfed12fc33ad9e66ebe5a870d1145ab
lunaroyster <oneinaquadrillion@live.com>
2019-09-13 22:18:48 -0400

    Better search and styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, createRef } from 'react';
 import MathJax from 'react-mathjax2';
 import classNames from 'classnames';
 
-import { Table, TableRow, TableCell, TableBody, Container } from '@material-ui/core';
+import { Table, TableRow, TableCell, TableBody, Container, TableHead } from '@material-ui/core';
 
 import './App.css';
 
@@ -12,6 +12,9 @@ import Fuse from 'fuse.js';
 
 const searchConfig = {
   keys: ['command', 'descriptions'],
+  includeMatches: true,
+  shouldSort: true,
+  minMatchCharLength: 2,
 };
 
 class App extends Component {
@@ -57,7 +60,7 @@ class App extends Component {
   pressKey = e => {
     if (e.key === 'Enter') {
       if (this.state.searchResult[this.state.selectedResult]) {
-        this.copyToClipboard(this.state.searchResult[this.state.selectedResult].command);
+        this.copyToClipboard(this.state.searchResult[this.state.selectedResult].item.command);
       }
     }
   }
@@ -79,6 +82,14 @@ class App extends Component {
     if (e.key === 'ArrowDown') this.selectNext();
     if (e.key === 'ArrowUp') this.selectPrevious();
   };
+  getVisibleDescriptions = (item, matches) => {
+    let descriptions = [];
+    for (let m of matches) {
+      if (m.key !== 'descriptions') continue;
+      descriptions.push(item.descriptions[m.arrayIndex]);
+    }
+    return descriptions;
+  }
   render() {
     let { searchTerm, selectedResult, searchResult } = this.state;
     return (
@@ -88,13 +99,23 @@ class App extends Component {
           <MathJax.Context input='tex'>
             <div>
               <Table>
+                <colgroup>
+                  <col style={{width:'25%'}}/>
+                  <col style={{width:'25%'}}/>
+                  <col style={{width:'25%'}}/>
+                  <col style={{width:'25%'}}/>
+                </colgroup>
                 <TableBody>
-                  {searchResult.map((r,i) => (
+                  {searchResult.map(({item: r, matches}, i) => (
                     <TableRow key={r.command} className={classNames('result', {'selected': i===selectedResult})} tabIndex={i+1}>
-                      <TableCell>{r.descriptions[0]}</TableCell>
-                      <TableCell><code style={{paddingRight: '1em'}}>{r.command}</code></TableCell>
-                      <TableCell><span className="renderedlatex"><MathJax.Node inline>{r.example}</MathJax.Node></span></TableCell>
-                      <TableCell>
+                      <TableCell colSpan={1}>
+                        {this.getVisibleDescriptions(r, matches).map(d => (
+                          <div key={d}>"{d}"</div>
+                        ))}
+                      </TableCell>
+                      <TableCell colSpan={1}><code style={{paddingRight: '1em'}}>{r.command}</code></TableCell>
+                      <TableCell colSpan={1}><span className="renderedlatex"><MathJax.Node inline>{r.example}</MathJax.Node></span></TableCell>
+                      <TableCell colSpan={1}>
                         {i===selectedResult && (<span>↵ to copy</span>)}
                       </TableCell>
                     </TableRow>
