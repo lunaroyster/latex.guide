c5bb78277542bf36668c9eb8ff99defa70eca369
lunaroyster <oneinaquadrillion@live.com>
2019-09-14 02:53:49 -0400

    A ton of commands

--- src/Commands.js
+++ src/Commands.js
@@ -34,7 +34,7 @@ export default [
   },{
     command: '\\neq',
     example: '\\neq',
-    descriptions: ['not equal to'],
+    descriptions: ['not equal to', '!='],
   },{
     command: '\\approx',
     example: '\\approx',
@@ -83,5 +83,125 @@ export default [
     command: '\\ll',
     example: '\\ll',
     descriptions: ['much less than'],
+  },{
+    command: '\\mathbb{R}',
+    example: '\\mathbb{R}',
+    descriptions: ['R', 'Real Numbers'],
+  },{
+    command: '\\mathbb{Q}',
+    example: '\\mathbb{Q}',
+    descriptions: ['Q', 'Rational Numbers'],
+  },{
+    command: '\\mathbb{Z}',
+    example: '\\mathbb{Z}',
+    descriptions: ['Z', 'Integers'],
+  },{
+    command: '\\mathbb{N}',
+    example: '\\mathbb{N}',
+    descriptions: ['N', 'Natural Numbers'],
+  },{
+    command: '\\circ',
+    example: 'g \\circ f',
+    descriptions: ['Function Composition', 'g(f(x))', 'g of f'],
+  },{
+    command: '\\square',
+    example: '\\square',
+    descriptions: ['Square'],
+  },{
+    command: '\\blacksquare',
+    example: '\\blacksquare',
+    descriptions: ['Black Square'],
+  },{
+    command: '\\wedge',
+    example: 'P \\wedge Q',
+    descriptions: ['And'],
+  },{
+    command: '\\vee',
+    example: 'P \\vee Q',
+    descriptions: ['Or'],
+  },{
+    command: '\\neg{}',
+    example: '\\neg{P}',
+    descriptions: ['Negation', 'Not'],
+  },{
+    command: '\\implies',
+    example: 'P \\implies Q',
+    descriptions: ['Implies', 'If P, then Q'],
+  },{
+    command: '\\iff',
+    example: 'P \\iff Q',
+    descriptions: ['If and only if', 'Implication'],
+  },{
+    command: '\\sin{}',
+    example: '\\sin{x}',
+    descriptions: ['sin', 'sine'],
+  },{
+    command: '\\cos{}',
+    example: '\\cos{x}',
+    descriptions: ['cos', 'cosine'],
+  },{
+    command: '\\tan{}',
+    example: '\\tan{x}',
+    descriptions: ['tan', 'tangent'],
+  },{
+    command: '\\csc{}',
+    example: '\\csc{x}',
+    descriptions: ['csc', 'cosec', 'cosecant'],
+  },{
+    command: '\\sec{}',
+    example: '\\sec{x}',
+    descriptions: ['sec', 'secant'],
+  },{
+    command: '\\cot{}',
+    example: '\\cot{x}',
+    descriptions: ['cot', 'cotan', 'cotangent'],
+  },{
+    command: '\\in',
+    example: 'a \\in A',
+    descriptions: ['is member of', 'in', 'belongs to set'],
+  },{
+    command: '\\ni',
+    example: 'A \\ni a',
+    descriptions: ['owns', 'has member'],
+  },{
+    command: '\\notin',
+    example: 'a \\notin A',
+    descriptions: ['is not member of', 'not in', 'doesn\'t belong to set'],
+  },{
+    command: '\\subset',
+    example: 'a \\subset A',
+    descriptions: ['is subset of', 'proper subset'],
+  },{
+    command: '\\not\\subset',
+    example: 'a \\not\\subset A',
+    descriptions: ['is not subset of', 'not proper subset'],
+  },{
+    command: '\\subseteq',
+    example: 'a \\subseteq A',
+    descriptions: ['is subset or equivalent set', 'improper subset'],
+  },{
+    command: '\\nsubseteq',
+    example: 'a \\nsubseteq A',
+    descriptions: ['is not a subset or equivalent set', 'not improper subset'],
+  },{
+    command: '\\setminus',
+    example: '\\mathbb{R} \\setminus \\mathbb{Q}',
+    descriptions: ['subtract set'],
+  },{
+    command: '\\int_{a}^{b}',
+    example: '\\int_{a}^{b} f(s) ds',
+    descriptions: ['definite integral'],
+  },{
+    command: '\\int',
+    example: '\\int f(s) ds',
+    descriptions: ['indefinite integral'],
+  },{
+    command: '\\overline{}',
+    example: '\\overline{I}',
+    descriptions: ['Overline'],
+  },{
+    command: '\\underline{}',
+    example: '\\underline{I}',
+    descriptions: ['Underline'],
   },
 ]
